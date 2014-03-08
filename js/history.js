eca01ddb65eb268c06b6ce1a33ff6d96c9acd5ca
tomohitoy <t07840ty@gmail.com>
2014-03-09 01:45:28 +0900

    percentile plot version

--- js/history.js
+++ js/history.js
@@ -1,89 +1,18 @@
-var plotDonut = function(correct_num,summary_num){
-	var height = 300;
-	var width = 300;
-	var percentile = Math.round(correct_num / summary_num * 1000)/10;
-	
-	var oRadius = width/2;
-	var iRadius = 100;
-	
-	var arc = d3.svg.arc()
-				.innerRadius(iRadius)
-				.outerRadius(oRadius);
-	
-	var dataset = [correct_num,summary_num-correct_num];
-	var pie = d3.layout.pie();
-	var pieChart = d3.select('#graph_area')
-						.append('svg')
-						.attr({
-							'width':width,
-							'height':height
-						});
-	var color = ["#20F67D","red"];
-	var wedges = pieChart.selectAll('g')
-							.data(pie(dataset))
-							.enter()
-							.append('g')
-							.attr({
-								'class':'wedge',
-								'transform':'translate('+oRadius+','+oRadius+')'
-							});
-		wedges.append('path')
-				.attr({
-					'fill':function(d,i){
-						return color[i];
-					},
-					'd':arc,
-					'stroke':'white'
-				})
-				.transition()
-				.duration(500)
-				.attrTween("d", function(d){
-					var interpolate = d3.interpolate(
-						{ startAngle : 0, endAngle : 0 },
-						{ startAngle : d.startAngle, endAngle : d.endAngle }
-					);
-					return function(t){
-						return arc(interpolate(t));
-					}
-				});
-		if(summary_num == 0){
-			wedges.append('text')
-					.attr({
-						'font-size':'20pt',
-						'fill':'black',
-						'text-anchor':'middle'
-					})
-					.text("データはありません。");
-		}else{
-			wedges.append('text')
-					.attr({
-						'font-size':'30pt',
-						'fill':'black',
-						'text-anchor':'middle'
-					})
-					.text(percentile+"％");
-		}
-}
-
-
-var drawBargraph = function(dataset,selector,type){
-	var minDate = dataset[0];
-	var maxDate = dataset[dataset.length-1];
-	$('#description').html('<p><strong>'+minDate.year+'年'+minDate.month+'月'+minDate.day+'日〜'+maxDate.year+'年'+maxDate.month+'月'+maxDate.day+'日の'+type+'履歴</strong></p>');
-	$('#tooltip').hide();
+var drawPercentilegraph = function(dataset,selector){
 	var w = 400;
-	var h = 200;
-	var margin = {'top':0,'left':30,'right':0,'bottom':30};
+	var h = 250;
+	var margin = {'top':10,'left':40,'right':0,'bottom':30};
 	var inw = w - (margin.right + margin.left);
 	var inh = h - (margin.top + margin.bottom);
+	var formatPercent = d3.format('.0%')
 	var xScale = d3.scale
 					.ordinal()
 					.domain(d3.range(dataset.length))
-					.rangeRoundBands([margin.left,inw],0.05);
+					.rangeRoundBands([margin.left,inw],0.2);
 	var yScale = d3.scale
 					.linear()
-					.domain([0,d3.max(dataset,function(d){return d.counter;})])
-					.range([inh, 0]);
+					.domain([0,1])
+					.range([inh, margin.top]);
 	var xAxis = d3.svg
 					.axis()
 					.scale(xScale)
@@ -91,50 +20,45 @@ var drawBargraph = function(dataset,selector,type){
 	var yAxis = d3.svg
 					.axis()
 					.scale(yScale)
-					.orient('left');
+					.orient('left')
+					.ticks(10)
+					.tickFormat(formatPercent);
+	var tip = d3.tip()
+				.attr('class','d3-tip')
+				.offset([-10,0])
+				.html(function(d){
+					return '<strong>'+Math.round(d.percentile*1000)/10+'％</strong>('+d.summary+'問中'+d.correct+'問正解)';
+				});
 	var svg = d3.select(selector)
 				.append('svg')
 				.attr({
 					'width':w,
 					'height':h
 				});
+	svg.call(tip);
 	svg.selectAll('rect')
 		.data(dataset)
 		.enter()
 		.append('rect')
 		.attr({
 			'x': function(d,i){return xScale(i);},
-			'y': function(d){return yScale(d.counter);},
+			'y': function(d){return yScale(d.percentile);},
 			'width': xScale.rangeBand(),
-			'height': function(d){return h - yScale(d.counter) - margin.bottom;},
+			'height': function(d){return h - yScale(d.percentile) - margin.bottom - margin.top;},
 			'fill': 'orange'
 		})
-		.on('mouseover',function(d){
-			$('#description').hide();			
-			d3.select('#value')
-				.text(d.counter);
-			d3.select('#year')
-				.text(d.year);
-			d3.select('#month')
-				.text(d.month);
-			d3.select('#day')
-				.text(d.day);
-			$('#tooltip').show();
-		})
-		.on('mouseout',function(){
-			$('#tooltip').hide();
-			$('#description').show();
-		});
+		.on('mouseover',tip.show)
+		.on('mouseout',tip.hide)
 	svg.append('g')
 		.attr({
 			'class':'axis',
-			'transform':'translate(0,'+(h-margin.bottom)+')'
+			'transform':'translate(0,'+(h-margin.bottom-margin.top)+')'
 		})
 		.call(xAxis)
 		.selectAll('text')
 		.data(dataset)
 		.text(function(d){
-			return d.month+'月'+d.day+'日';
+			return d.start_month+'月'+d.start_day+'日〜';
 		});
 	svg.append('g')
 		.attr({
